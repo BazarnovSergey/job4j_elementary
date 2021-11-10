ca12b43b673aea77e5afb886ed5394fd5ddf1731
macbookpro <101sergey101@gmail.com>
2021-11-10 19:28:23 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Bazarnov");
+        System.out.println("29.12.1988");
+    }
+}
