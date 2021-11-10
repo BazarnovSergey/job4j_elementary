6f41abac3af2a4543dca3b9b3a38eecf973070d8
macbookpro <101sergey101@gmail.com>
2021-11-11 00:20:44 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,6 +1,10 @@
 package ru.job4j;
 
 public class Main {
+    int age = 180000000;
+    float length = 78.0332F;
+    boolean baby = true;
+
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
