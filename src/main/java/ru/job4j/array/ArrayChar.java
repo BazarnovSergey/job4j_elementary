5d60bfaea035df6201ad6344f01df4541a3df04a
macbookpro <101sergey101@gmail.com>
2021-11-21 13:30:52 +0300

    6.4. Слово начинается с ... [#41585 #231107]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,17 +2,9 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        if (word.length >= pref.length) {
-            for (int i = 0; i < pref.length; i++) {
-                if (word[i] != pref[i]) {
-                    return false;
-                }
-            }
-        } else {
-            for (int i = 0; i < word.length; i++) {
-                if (word[i] != pref[i]) {
-                    return false;
-                }
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                return false;
             }
         }
         return true;
